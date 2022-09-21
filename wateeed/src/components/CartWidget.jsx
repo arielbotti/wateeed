3902386a750bff0fa3d17f157e0b797d6284b770
Ariel Botti <ariel.botti@outlook.com>
2022-09-20 21:38:33 -0300

    clase 4

---
+++ wateeed/src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartWidget = () => {
+    return (
+    
+    <i className="material-icons">shopping_cart</i>
+      
+    );
+}
+
+export default CartWidget;
---
+++ wateeed/src/components/ItemListContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ItemListContainer = () => {
+    return (
+        
+        
+        <div className="card border-info mb-3" style={{maxWidth: '20rem'}}>
+            <div className="card-header">Carrito</div>
+            <div className="card-body">
+                <h4 className="card-title">Articulos</h4>
+                <p className="card-text">Valvula N/A -- 1 Unidad</p>
+            </div>
+        </div>
+        
+    );
+}
+
+export default ItemListContainer;

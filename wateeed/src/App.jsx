9508af83488cd9f3ed9a23666f6b53974b315ee6
Ariel Botti <ariel.botti@outlook.com>
2022-09-29 20:01:01 -0300

    product component

--- wateeed/src/App.jsx
+++ wateeed/src/App.jsx
@@ -1,16 +1,25 @@
 // usar LITERA Bootswatch
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import Product from './components/Product';
+import Footer from './components/Footer';
 
 
 
 function App() {
   return (
     <>
-      <NavBar/>
-      <ItemListContainer/>
+      <BrowserRouter/>
+        <NavBar/>
+        <Routes>
+          <Route element={<Product/>} path="/product" />
+        </Routes>
+        <Footer/>
+      <BrowserRouter/>
     </>
 
   );
